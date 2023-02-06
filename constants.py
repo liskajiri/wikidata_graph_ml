8209d20356eaa3069dc0da4f6ca7515982146694
Jiri Liska <liskaj@msoe.edu>
2023-02-06 17:51:23 -0600

    Created a Pytorch Dataset class for easy parsing

---
+++ constants.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+dataset_folder = "datasets/"
+
+class DatasetWebPaths(Enum):
+    transductive = "https://www.dropbox.com/s/6sbhm0rwo4l73jq/wikidata5m_transductive.tar.gz?dl=1"
+    corpus = "https://www.dropbox.com/s/7jp4ib8zo3i6m10/wikidata5m_text.txt.gz?dl=1"
+    entity_aliases = "https://www.dropbox.com/s/lnbhc8yuhit4wm5/wikidata5m_alias.tar.gz?dl=1"
+
+class DatasetPaths(Enum):
+    train = dataset_folder + "wikidata5m_transductive_train.txt"
+    validate = dataset_folder + "wikidata5m_transductive_valid.txt"
+    test = dataset_folder + "wikidata5m_transductive_test.txt"
+    corpus = dataset_folder + "wikidata5m_text.txt"
