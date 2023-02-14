95d67c5a422e808bc3a08d428a8fd1527aad2019
Jiri Liska <liskaj@msoe.edu>
2023-02-14 11:31:50 -0600

    Fixed filename constants

--- constants.py
+++ constants.py
@@ -1,7 +1,5 @@
 from enum import Enum
 
-dataset_folder = "datasets/"
-
 class DatasetWebPaths(Enum):
     transductive = "https://www.dropbox.com/s/6sbhm0rwo4l73jq/wikidata5m_transductive.tar.gz?dl=1"
     corpus = "https://www.dropbox.com/s/7jp4ib8zo3i6m10/wikidata5m_text.txt.gz?dl=1"
@@ -12,9 +10,8 @@ class DatasetTarNames(Enum):
     corpus = "wikidata5m_text.txt.gz"
     entity_aliases = "wikidata5m_alias.tar.gz"
 
-class DatasetPaths(Enum):
-    train = dataset_folder + "wikidata5m_transductive_train.txt"
-    validate = dataset_folder + "wikidata5m_transductive_valid.txt"
-    test = dataset_folder + "wikidata5m_transductive_test.txt"
-    corpus = dataset_folder + "wikidata5m_text.txt"
-    utf8_text_file_path = dataset_folder + "wikidata5m_text_utf8.csv"
+class DatasetNames(Enum):
+    train = "wikidata5m_transductive_train.txt"
+    validate = "wikidata5m_transductive_valid.txt"
+    test = "wikidata5m_transductive_test.txt"
+    corpus = "wikidata5m_text.txt"
